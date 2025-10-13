8feb9a1f659dc324b14d61f54e7e699d46e92c56
EnzoAguirre04 <ea.aguirre@alumno.um.edu.ar>
2025-10-12 22:20:51 -0300

    Actualización del README.md y creación de test_game.

---
+++ tests/test_game.py
@@ -0,0 +1,7 @@
+"""
+Computación I - Backgammon.
+Nombre y Apellido: Enzo Agustín Aguirre Polenta.
+Ciclo Lectivo: 2025.
+Carrera: Ingeniería en Informática.
+Ruta: "computacion-2025-backgammon-EnzoAguirre04/tests/test_game.py".
+"""
